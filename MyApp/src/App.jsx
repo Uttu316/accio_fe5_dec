f5fb11fc1f5c8f02dc5b754effa25085cce1bc94
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2024-12-15 23:27:59 +0530

    React Project

---
+++ MyApp/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+const App = () => {
+  let x = 2;
+  let y = 4;
+  return (
+    <div className="box">
+      <h1>Welcome to React</h1>
+      <ul>
+        <li>Swetha</li>
+        <li>Raju</li>
+        <li>Vaibhav</li>
+        <li>Ravi</li>
+        <li>Afreen</li>
+      </ul>
+      Result: {x + y}
+    </div>
+  );
+};
+
+export default App;
+
+export const a = 2;
